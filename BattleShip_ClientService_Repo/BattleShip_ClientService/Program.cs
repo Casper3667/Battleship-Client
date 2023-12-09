1bd02d80d272bf3ac28b5cb6df6f8c1834246495
Kalina <10666@student.eadania.dk>
2023-12-09 12:54:04 +0100

    GameServerCommunication_Version-3.1.0
    This Version Doesnt Use Incoded IP And Port for Game Server adress but gets it From Game Lobby
    The reason for there not being much if any new code, is because Client Side was Capable in GameServerCommunication Version 2.1.0  BUT since Game Lobby had issues there was also code then to Circumvent GameLobby, thus this is a version where that isnt an option any longer
    
    Tasks:
    
    -  #36

--- BattleShip_ClientService_Repo/BattleShip_ClientService/Program.cs
+++ BattleShip_ClientService_Repo/BattleShip_ClientService/Program.cs
@@ -2,6 +2,7 @@
 using BattleShip_ClientService.Interfaces;
 using BattleShip_ClientService.Settings;
 using System.Diagnostics;
+const string validTokenforTesting = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiVXNlcm5hbWUiLCJleHAiOjE3MDIyMDY3MjZ9.8GQbviPdgD8J3iwgpqhS39splxsmbnE5IwJl4_Q-dd0";
 
 Settings.LoadSettings();
 //Console.WriteLine("Hello, World!");
@@ -9,19 +10,19 @@ LoginServiceInterface loginServiceInterface = new LoginServiceInterface();
 LobbyServiceInterface lobbyServiceInterface = new LobbyServiceInterface();
 GameServerInterface gameServerInterface = new GameServerInterface();
 
-    Task<string> loginTask= loginServiceInterface.LoginScreen();
+Task<string> loginTask = loginServiceInterface.LoginScreen();
 //loginTask.Start();
 loginTask.Wait();
-if(loginTask.IsCompletedSuccessfully)
+if (loginTask.IsCompletedSuccessfully)
 {
-    string token=loginTask.Result;
+    string token = loginTask.Result;
 
     Debug.WriteLine("After LoginScreen\nToken: " + token);
-    
+
     ServerAdress? adress = lobbyServiceInterface.JoinLobby(token);
-    if(adress != null)
+    if (adress != null)
     {
-        Console.WriteLine("After Game Server Lobby\nGame Server Adress:\nIP: " + adress.IP+"\nPort: "+adress.Port);
+        Console.WriteLine("After Game Server Lobby\nGame Server Adress:\nIP: " + adress.IP + "\nPort: " + adress.Port);
         Debug.WriteLine("After Game Server Lobby\nGame Server Adress:\nIP: " + adress.IP + "\nPort: " + adress.Port);
         gameServerInterface.Run(adress, token);
     }
@@ -29,9 +30,9 @@ if(loginTask.IsCompletedSuccessfully)
     {
         Console.WriteLine("Didn't Connect");
         Debug.WriteLine("Didn't Connect");
-        Debug.WriteLine("Trying To Connect Anyway");
-        ServerAdress temp=new ServerAdress() { IP = Settings.NetworkSettings.LobbyIP,Port=13000};
-        gameServerInterface.Run(temp, token);
+        //Debug.WriteLine("Trying To Connect Anyway");
+        //ServerAdress temp=new ServerAdress() { IP = Settings.NetworkSettings.LobbyIP,Port=13000};
+        //gameServerInterface.Run(temp, token);
     }
-   
+
 }
